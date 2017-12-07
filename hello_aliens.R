4d10c95ca92ab1c441e51ee27db468dbf963249d
Maria Rodriguez <mad100141@gmail.com>
2017-12-07 18:17:11 -0500

    Hello Aliens

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+cat("Well, this is awkward. Hi there Aliens, or do you prefer extraterrestials? Friends? We're all friends right?
+    Friends don't kill each other just to let you know. Nor do we kidnap one another and probe them. So no probing.
+    Yeah, why am I the one to talk. Where's Obama when you need him? Thanks Obama.")
+
